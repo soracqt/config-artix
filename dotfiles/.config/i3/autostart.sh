20fbc016affee08456a98d59123e9d5c8a812916
soracqt <72482919+soracqt@users.noreply.github.com>
2022-02-25 20:32:23 +0900

    update autostart.sh

--- dotfiles/.config/i3/autostart.sh
+++ dotfiles/.config/i3/autostart.sh
@@ -7,7 +7,7 @@ gammastep -m randr &
 /usr/bin/fcitx5 &
 /usr/lib/kdeconnectd &
 keepassxc --localconfig $HOME/.config/keepassxc/keepassxc.ini &
-xss-lock --transfer-sleep-lock -- i3lock --nofork -c 000000 &
+xss-lock -n dim-screen.sh -- i3lock -n &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets &
