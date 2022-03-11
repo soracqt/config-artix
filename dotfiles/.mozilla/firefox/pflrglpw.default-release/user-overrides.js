4d263fbe871471ef3c6f3fef59f6bd2c15cf5618
soracqt <72482919+soracqt@users.noreply.github.com>
2022-03-11 20:12:57 +0900

    update firefox conf

---
+++ dotfiles/.mozilla/firefox/pflrglpw.default-release/user-overrides.js
@@ -0,0 +1,5 @@
+user_pref("media.av1.enabled", false);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("dom.allow_cut_copy", true);
+user_pref("dom.event.clipboardevents.enabled", true);
