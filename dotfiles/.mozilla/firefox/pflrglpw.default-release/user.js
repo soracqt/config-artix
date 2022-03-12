5764dc705132737babba4b6d16a9a95d3f9bb90d
soracqt <72482919+soracqt@users.noreply.github.com>
2022-03-12 11:23:16 +0900

    update

--- dotfiles/.mozilla/firefox/pflrglpw.default-release/user.js
+++ dotfiles/.mozilla/firefox/pflrglpw.default-release/user.js
@@ -1368,5 +1368,10 @@ user_pref("network.http.referer.XOriginPolicy", 0);
 // Enable copy and paste on web eg. github, scrapbox
 user_pref("dom.allow_cut_copy", true);
 user_pref("dom.event.clipboardevents.enabled", true);
-// https://github.com/arkenfox/user.js/wiki/3.2-Overrides-%5BCommon%5D#-fingerprinting
+// 4504
 user_pref("privacy.resistFingerprinting.letterboxing", false);
+// 0801
+user_pref("keyword.enabled", true);
+/* override recipe: enable DRM and let me watch videos ***/
+   // user_pref("media.gmp-widevinecdm.enabled", true); // 2021 default-inactive in user.js
+user_pref("media.eme.enabled", true); // 2022
