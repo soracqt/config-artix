d987b6386f552389024ec2360fcab0e927e0a11f
soracqt <72482919+soracqt@users.noreply.github.com>
2022-03-12 11:00:02 +0900

    update

--- dotfiles/.mozilla/firefox/pflrglpw.default-release/user-overrides.js
+++ dotfiles/.mozilla/firefox/pflrglpw.default-release/user-overrides.js
@@ -1,5 +1,12 @@
-user_pref("media.av1.enabled", false);
+// To enable VA-API
+// https://wiki.archlinux.org/title/firefox#Hardware_video_acceleration
 user_pref("media.ffmpeg.vaapi.enabled", true);
+// Disable AV1 decoding
+user_pref("media.av1.enabled", false);
+// To get working pixiv.net
 user_pref("network.http.referer.XOriginPolicy", 0);
+// Enable copy and paste on web eg. github, scrapbox
 user_pref("dom.allow_cut_copy", true);
 user_pref("dom.event.clipboardevents.enabled", true);
+// https://github.com/arkenfox/user.js/wiki/3.2-Overrides-%5BCommon%5D#-fingerprinting
+user_pref("privacy.resistFingerprinting.letterboxing", false);
--- dotfiles/.mozilla/firefox/pflrglpw.default-release/user.js
+++ dotfiles/.mozilla/firefox/pflrglpw.default-release/user.js
@@ -1358,8 +1358,15 @@ user_pref("app.update.background.scheduling.enabled", false);
 /* END: internal custom pref to test for syntax errors ***/
 user_pref("_user.js.parrot", "SUCCESS: No no he's not dead, he's, he's restin'!");
 
-user_pref("media.av1.enabled", false);
+// To enable VA-API
+// https://wiki.archlinux.org/title/firefox#Hardware_video_acceleration
 user_pref("media.ffmpeg.vaapi.enabled", true);
+// Disable AV1 decoding
+user_pref("media.av1.enabled", false);
+// To get working pixiv.net
 user_pref("network.http.referer.XOriginPolicy", 0);
+// Enable copy and paste on web eg. github, scrapbox
 user_pref("dom.allow_cut_copy", true);
 user_pref("dom.event.clipboardevents.enabled", true);
+// https://github.com/arkenfox/user.js/wiki/3.2-Overrides-%5BCommon%5D#-fingerprinting
+user_pref("privacy.resistFingerprinting.letterboxing", false);
