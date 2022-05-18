b8bbb8804cb870929d01e1aa04213ba513f2d36a
soracqt <72482919+soracqt@users.noreply.github.com>
2022-05-18 14:20:51 +0900

    upd

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-set -uo pipefail
+set -euo pipefail
 sudo cp -r etc/ /
 sudo cp -r usr/ /
 rsync -av dotfiles/. ~
 cat fstab|sudo tee -a /etc/fstab
 sudo chattr +i /etc/resolv.conf
-sudo chmod 700 /boot /etc/{iptables,arptables}
+sudo chmod 700 /boot /etc/{iptables,arptables} || true
 echo "Defaults editor=/usr/bin/rvim"|sudo tee -a /etc/sudoers
-sudo pacman -S virt-manager
+sudo pacman -S virt-manager --noconfirm
 sudo gpasswd -a $USER libvirt
