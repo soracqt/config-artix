dd8a6a2d07e42fba2bbe5dc23d8ec156fe41ca21
soracqt <72482919+soracqt@users.noreply.github.com>
2022-07-17 20:13:53 +0900

    Rm sudoers setup line

--- setup.sh
+++ setup.sh
@@ -7,6 +7,5 @@ echo -e 'tmpfs   /tmp         tmpfs   rw,nodev,nosuid,size=4G          0  0
 tmpfs   /home/takumi/.cache      tmpfs   noatime,nodev,nosuid,size=2G  0       0'|sudo tee -a /etc/fstab
 sudo chattr +i /etc/resolv.conf
 sudo chmod 700 /boot /etc/{iptables,arptables} || true
-echo "Defaults editor=/usr/bin/rvim"|sudo tee -a /etc/sudoers
 sudo pacman -S virt-manager --noconfirm
 sudo gpasswd -a $USER libvirt
